60a2b5deae41ed322d5b39844c5f54506e3dfe1f
zhaochenxi <zhaochenxi@172.16.5.66>
2016-12-13 10:29:51 +0800

    first commit

---
+++ boots/src/main/java/com/boots/controller/package-info.java
@@ -0,0 +1,13 @@
+/**   
+ * @Title: package-info.java 
+ * @Description: TODO(用一句话描述该文件做什么) 
+ * @author zhaochenxi   
+ * @date 2016年12月13日 上午10:08:34 
+*/
+/** 
+ * @ClassName: package-info 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author zhaochenxi
+ * @date 2016年12月13日 上午10:08:34 
+ */
+package com.boots.controller;
